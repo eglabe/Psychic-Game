84cedb260a04a617bb7c84bf6100a314af6beb5a
Erin <eringlabe@gmail.com>
2017-01-30 18:34:46 -0800

    done! I think

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,70 @@
 var wins = 0;
 var losses = 0;
 var guessLeft = 9;
+var letters = []; //letters guessed
 
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+
+
+function reset () {
+	guessLeft = 9;
+	letters = [];
+	computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	leftHTML = "Guesses Left : " + guessLeft;
+	lettersHTML = "Letters Guessed: " + letters;
+
+	document.querySelector("#left").innerHTML = leftHTML;
+	document.querySelector("#letters").innerHTML = lettersHTML;
+}
+
+//the function which runs when user pushes a key
 document.onkeydown = function(guess) {
 
 	var userGuess = guess.key;
-	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	var letters = new Array(); //letters the user has picked
 
+	//if the user guesses correctly
 	if (userGuess == computerGuess) {
 		wins++;
-		//game resets
-	}
-		else {
-			guessLeft--;
-			letters.push(guess.key);//pushes all of the letters
 
+		alert("You won!");
+
+		reset();
+	}
+
+
+	//if the user guesses a unique letter
+	if (letters.indexOf(userGuess) == -1) {
+			letters.push(guess.key);
+			guessLeft--;
+	}
+
+	//if the user reaches the guess limit and loses
 	if (guessLeft == 0) {
 		losses++;
-		//game resets
+
+		alert("You lost...the letter was " + computerGuess);
+
+		reset();
 	}
-}
-
-//Inserts variable values into DOM
-var winsHTML = "Wins: " + wins;
-document.querySelector("#wins").innerHTML = winsHTML;
-
-var lossesHTML = "Losses: " + losses;
-document.querySelector("#losses").innerHTML = lossesHTML;
-
-var leftHTML = "Guesses Left : " + guessLeft;
-document.querySelector("#left").innerHTML = leftHTML;
-
-var lettersHTML = "Letters Guessed: " + letters;
-document.querySelector("#letters").innerHTML = lettersHTML;
 
 
+	//Inserts variable values into DOM
+	var winsHTML = "Wins: " + wins;
+	document.querySelector("#wins").innerHTML = winsHTML;
 
-//console.log(userGuess);
-//console.log(computerGuess);
-console.log(letters);
+	var lossesHTML = "Losses: " + losses;
+	document.querySelector("#losses").innerHTML = lossesHTML;
+
+	var leftHTML = "Guesses Left : " + guessLeft;
+	document.querySelector("#left").innerHTML = leftHTML;
+
+	var lettersHTML = "Letters Guessed: " + letters;
+	document.querySelector("#letters").innerHTML = lettersHTML;
 
 }
 
-//when a key is pressed:
-	//computer picks a letter
-
-	//if userGuess == computerGuess 
-		//wins++
-		//game restarts
-
-	//else	
-	//guessLeft--
-	//letters is logged
-
-	//if guessLeft = 0
-		//game restarts
 
 
 
